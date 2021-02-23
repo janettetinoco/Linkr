eea6cfc11929ca7530ea1477f8c79d06d1e1faf2
AlexeyS <xengawe@gmail.com>
2021-02-23 09:32:17 -0800

    fix fetch users by city

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -3,6 +3,7 @@ import { RECEIVE_USERS } from '../actions/user_actions';
 const initialState = {};
 
 export default function(state = initialState, action) {
+  Object.freeze(state)
   let nextState = Object.assign({}, state);
 
   switch (action.type) {
