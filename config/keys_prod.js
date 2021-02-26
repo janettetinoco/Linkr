a8f35f26ff06a48cdb1237baaa7f31fb8442480a
Janette <tinoconjanette@gmail.com>
2021-02-26 15:15:01 -0800

    ready to deploy again

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,7 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.SECRET_OR_KEY
+    secretOrKey: process.env.SECRET_OR_KEY,
+    AWSaccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    AWSsecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+
 }
