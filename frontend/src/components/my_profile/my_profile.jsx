39fc5bad8d3ef4c03e4b5ab4c5f50f6de980fab1
Michael Clayton Noble <makos.code@gmail.com>
2021-02-25 16:52:22 -0800

    profile my profile

---
+++ frontend/src/components/my_profile/my_profile.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+class MyProfile extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+      <div id="profile">
+        <div id="header">
+          <div id="img">
+            {/* <img src={this.props.user.image_url} /> */}
+            <hr/>
+            I<br />
+            &emsp;M<br />
+            &emsp;&emsp;A<br />
+            &emsp;&emsp;&emsp;G<br />
+            &emsp;&emsp;&emsp;&emsp;E<br />
+            <hr/>
+          </div>
+          name
+          <h1>{this.props.user.name}</h1>
+          <span>{this.props.user.city}</span>
+          <span></span>
+        </div>
+        <div id="body">
+          <div id="stats">
+
+            <div className="details">
+              <p>Education</p>
+              
+            </div>
+
+            <div className="details">
+              <p>Occupation</p>
+              {this.props.user.occupation}
+            </div>
+
+            <div className="details">
+              <p>Industry</p>
+              {this.props.user.industry}
+            </div>
+          </div>
+          <div id="about-me">
+            {this.props.user.aboutMe}
+          </div>
+
+          <div id="linkedIn">
+            <Link to={this.props.user.linkedIn}>linkedIn</Link>
+          </div>
+        </div>
+      </div>
+  }
+}
---
+++ frontend/src/components/my_profile/my_profile_container.js
@@ -0,0 +1,9 @@
+
+import {connect} from 'react-redux'; 
+
+//mstp 
+//    self: state.users[state.session.user.id],
+
+
+//mdtp
+
