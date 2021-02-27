75bc9e183a146e9926d7445b2c8339d63978e160
AlexeyS <xengawe@gmail.com>
2021-02-26 18:55:22 -0800

    add more variables to random user

--- seed/faker_seeds.js
+++ seed/faker_seeds.js
@@ -10,17 +10,38 @@ for (i = 0; i < 20; i++){
   let lastName = faker.name.lastName(); // Rowan Nikolaus
   let fullName = firstName + " " + lastName
   user.name = fullName;
-
+  
+  //create email
   let email = "";
   user.name.split(" ").forEach((ele, i) => {
-      if (i === 0){
-        email += ele.toLowerCase()
-      } else if (i === 1){
-        email += ele[0].toLowerCase() + '@' + firstName[0].toLowerCase() + ele[0].toLowerCase() + '.com'
-        user.email = email
-      } 
-    })
-    arr.push(user)
+    if (i === 0){
+      email += ele.toLowerCase()
+    } else if (i === 1){
+      email += ele[0].toLowerCase() + '@' + firstName[0].toLowerCase() + ele[0].toLowerCase() + '.com'
+      user.email = email
+    } 
+  
+  })
+  //create password
+  user.password = '123456'
+
+  //create industry
+  let industry = ["Space Exploration", "Biotech", "Software Engineering", "Political Science", "Minerals"]
+  user.industry = industry[Math.floor(Math.random() * industry.length)]
+
+  //create occupation
+  // let occupation = ["CTO", "CEO", "HR", "Accountant", "Engineer", "SWE", "QA"]
+  // user.occupation = occupation[Math.floor(Math.random() * occupation.length)]
+  // user.job = faker.name.jobArea()
+  user.occupation = faker.name.jobType()
+
+  //create city
+  let city = ["New York", "San Francisco", "Dallas"]
+  user.city = city[Math.floor(Math.random() * city.length)]
+
+
+
+  arr.push(user)
 }
 
 console.log(arr)
