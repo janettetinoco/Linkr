9532af0fe6d5c42b883d325b34e4a765f820c3e9
AlexeyS <xengawe@gmail.com>
2021-02-27 12:36:12 -0800

    work on async

--- seed/faker_seeds.js
+++ seed/faker_seeds.js
@@ -1,133 +1,136 @@
 const faker = require('faker')
 const detect = require('detect-gender');
 const getGenderInfo =  require('@nelsonomuto/gender-info');
-
-
-let arr = []
-let usedPics = [];
-
-for (i = 0; i < 20; i++){
-  let user = {}
-
-  let firstName = faker.name.firstName(); // Rowan Nikolaus
-  let lastName = faker.name.lastName(); // Rowan Nikolaus
-  let fullName = firstName + " " + lastName
-  user.name = fullName;
-  
-  //create email
-  let email = "";
-  user.name.split(" ").forEach((ele, i) => {
-    if (i === 0){
-      email += ele.toLowerCase()
-    } else if (i === 1){
-      email += ele[0].toLowerCase() + '@' + firstName[0].toLowerCase() + ele[0].toLowerCase() + '.com'
-      user.email = email
-    } 
-  
-  })
-  //create password
-  user.password = '123456'
-
-  //create industry
-  let industry = ["Space Exploration", "Biotech", "Software Engineering", 
-    "Political Science", "Minerals"]
-  user.industry = industry[Math.floor(Math.random() * industry.length)]
-
-  //create occupation
-  // user.jobtitle = faker.name.jobTitle()
-  let occupation = ["CTO", "CEO", "HR", "Accountant", "Engineer", "SWE", "QA",
-    "Assistant", "Agent", "Coordinator", "Developer","Backend Developer", "Full Stack",
-    "Frontend Developer"]
-  user.occupation = occupation[Math.floor(Math.random() * occupation.length)]
-
-  //create city
-  let city = ["New York", "San Francisco", "Dallas"]
-  user.city = city[Math.floor(Math.random() * city.length)]
-
-  //create education
-  let education = ["Davis", "SUSU", "FSU", "ASU", "SFSU", "UCLA", "Cal Poly", 
-    "CSUS", "FGCU", "Georgia Tech", "Hopkins", "JSU", "LA Tech", "Madison"]
-  user.education = education[Math.floor(Math.random() * education.length)]
-
-  //linkedIN
-  user.linkedIn = 'http://linkedIn.com/some_random_id';
-
-  //aboutMe
-  let aboutMe = ["Hello, I am looking for new friends.", "Hello, how are you?", 
-    "Hello, how are you?", "Hello, tell me a joke.", "Hello, I love running.", 
-    "Hello, I am cat person.", "Hello, I am dog person",  "Hello, I am foody.", 
-    "Hello, I am musician", "Hello, I am optimist.", "Hello, lets go for a walk.", 
-    "Hello, I love water.", "Hello, I here. Add me.",  "Hello, I am new to this.",
-    "Hello, I am excited.", "Hello, I am not alone :D", "I am feeling good today"]
-  user.aboutMe = aboutMe[Math.floor(Math.random() * aboutMe.length)]
-
-  //recruiterStatus
-  let recruiterStatus = [true, false]
-  user.recruiterStatus = recruiterStatus[Math.floor(Math.random() * recruiterStatus.length)]
-
-  //imageUrl
-  let femaleArr = ["https://linkr-dev.s3-us-west-1.amazonaws.com/michelle-obama.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-10.gif",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-11.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-12.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-13.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-14.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-15.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-4.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-5.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-6.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-7.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-8.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-9.jpg"
-  ]
-
-  let maleArr = ["https://linkr-dev.s3-us-west-1.amazonaws.com/batrrack%3Dobama.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-10.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-11.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-12.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-13.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-14.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-4.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-5.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-6.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-7.jpg",
-    "https://linkr-dev.s3-us-west-1.amazonaws.com/man8.jpg]",
-  ]
-
-
-  //gender check and insert photo url
-
-  // detect(user.name.split(" ")[0]).then(function (gender) {
-  //   console.log(gender)
-  // });
-  
-
-  if (getGenderInfo(user.name.split(" ")[0]).male === true ) {
-    if (maleArr.length) {
-      let image = maleArr[Math.floor(Math.random() * maleArr.length)]
-      if (!usedPics.includes(image)){
-        usedPics.push(image);
-        let idx = maleArr.indexOf(image)
-        maleArr.splice(idx, 1)
-        user.imageUrl = image
-        arr.push(user)
-      }
-    } 
-  } else {
-    if (femaleArr.length) {
-      let image = femaleArr[Math.floor(Math.random() * femaleArr.length)]
-      if (!usedPics.includes(image)){
-        let idx = femaleArr.indexOf(image)
-        femaleArr.splice(idx, 1)
-        user.imageUrl = image
-        arr.push(user)
-      }
-    }
-  }
-}
+const bcrypt = require('bcryptjs');
+const User = require('../models/User') // Link to User model 
 
 
 function doSeeding() {
+
+  let arr = []
+  let usedPics = [];
+
+  for (i = 0; i < 20; i++){
+    let user = {}
+
+    let firstName = faker.name.firstName(); // Rowan Nikolaus
+    let lastName = faker.name.lastName(); // Rowan Nikolaus
+    let fullName = firstName + " " + lastName
+    user.name = fullName;
+    
+    //create email
+    let email = "";
+    user.name.split(" ").forEach((ele, i) => {
+      if (i === 0){
+        email += ele.toLowerCase()
+      } else if (i === 1){
+        email += ele[0].toLowerCase() + '@' + firstName[0].toLowerCase() + ele[0].toLowerCase() + '.com'
+        user.email = email
+      } 
+    
+    })
+    //create password
+    user.password = '123456'
+
+    //create industry
+    let industry = ["Space Exploration", "Biotech", "Software Engineering", 
+      "Political Science", "Minerals"]
+    user.industry = industry[Math.floor(Math.random() * industry.length)]
+
+    //create occupation
+    // user.jobtitle = faker.name.jobTitle()
+    let occupation = ["CTO", "CEO", "HR", "Accountant", "Engineer", "SWE", "QA",
+      "Assistant", "Agent", "Coordinator", "Developer","Backend Developer", "Full Stack",
+      "Frontend Developer"]
+    user.occupation = occupation[Math.floor(Math.random() * occupation.length)]
+
+    //create city
+    let city = ["New York", "San Francisco", "Dallas"]
+    user.city = city[Math.floor(Math.random() * city.length)]
+
+    //create education
+    let education = ["Davis", "SUSU", "FSU", "ASU", "SFSU", "UCLA", "Cal Poly", 
+      "CSUS", "FGCU", "Georgia Tech", "Hopkins", "JSU", "LA Tech", "Madison"]
+    user.education = education[Math.floor(Math.random() * education.length)]
+
+    //linkedIN
+    user.linkedIn = 'http://linkedIn.com/some_random_id';
+
+    //aboutMe
+    let aboutMe = ["Hello, I am looking for new friends.", "Hello, how are you?", 
+      "Hello, how are you?", "Hello, tell me a joke.", "Hello, I love running.", 
+      "Hello, I am cat person.", "Hello, I am dog person",  "Hello, I am foody.", 
+      "Hello, I am musician", "Hello, I am optimist.", "Hello, lets go for a walk.", 
+      "Hello, I love water.", "Hello, I here. Add me.",  "Hello, I am new to this.",
+      "Hello, I am excited.", "Hello, I am not alone :D", "I am feeling good today"]
+    user.aboutMe = aboutMe[Math.floor(Math.random() * aboutMe.length)]
+
+    //recruiterStatus
+    let recruiterStatus = [true, false]
+    user.recruiterStatus = recruiterStatus[Math.floor(Math.random() * recruiterStatus.length)]
+
+    //imageUrl
+    let femaleArr = ["https://linkr-dev.s3-us-west-1.amazonaws.com/michelle-obama.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-10.gif",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-11.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-12.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-13.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-14.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-15.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-4.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-5.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-6.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-7.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-8.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-9.jpg"
+    ]
+
+    let maleArr = ["https://linkr-dev.s3-us-west-1.amazonaws.com/batrrack%3Dobama.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/man-10.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/man-11.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/man-12.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/man-13.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/man-14.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/man-4.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/man-5.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/man-6.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/man-7.jpg",
+      "https://linkr-dev.s3-us-west-1.amazonaws.com/man8.jpg]",
+    ]
+
+
+    //gender check and insert photo url
+
+    // detect(user.name.split(" ")[0]).then(function (gender) {
+    //   console.log(gender)
+    // });
+    
+
+    if (getGenderInfo(user.name.split(" ")[0]).male === true ) {
+      if (maleArr.length) {
+        let image = maleArr[Math.floor(Math.random() * maleArr.length)]
+        if (!usedPics.includes(image)){
+          usedPics.push(image);
+          let idx = maleArr.indexOf(image)
+          maleArr.splice(idx, 1)
+          user.imageUrl = image
+          arr.push(user)
+        }
+      } 
+    } else {
+      if (femaleArr.length) {
+        let image = femaleArr[Math.floor(Math.random() * femaleArr.length)]
+        if (!usedPics.includes(image)){
+          let idx = femaleArr.indexOf(image)
+          femaleArr.splice(idx, 1)
+          user.imageUrl = image
+          arr.push(user)
+        }
+      }
+    }
+  }
+
+
   
   //bcrypt the password & save to DB
   let newUser;
@@ -141,6 +144,7 @@ function doSeeding() {
         })
       })
   })
+  
 }
 
 
--- seed/seed_users.js
+++ seed/seed_users.js
@@ -1,43 +1,61 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User') // Link to User model 
 
-function doSeeds() {
+// function doSeeds() {
 
   //all the user` seeds
-   let users = [
-    { "name": "Jonathan Diaz", "email": "jonathand@j.com", "password": "123456", "industry": "Ape Industries", "recruiterStatus": false, "city": "New York" },
-    { "name": "Cindy Vega", "email": "cindyv@c.com", "password": "123456", "industry": "Software Engineering", "recruiterStatus": false, "city": "New York" },
-    { "name": "George Washington", "email": "georgew@g.com", "password": "123456", "industry": "Wood Chopping", "recruiterStatus": true, "city": "San Francisco" },
-    { "name": "Michelle Obama", "email": "michelleo@m.com", "password": "123456","industry": "Political Science", "recruiterStatus": false, "city": "New York" },
-    { "name": "Thomas Jefferson", "email": "thomasj@t.com", "password": "123456","industry": "Iron", "recruiterStatus": false, "city": "Dallas" },
-    { "name": "Martha Stewart", "email": "marthas@m.com", "password": "123456", "industry": "Food Industry", "recruiterStatus": false, "city": "Dallas" },
-    { "name": "Erick Guzi", "email": "erickg@e.com", "password": "123456", "industry": "Pharma", "recruiterStatus": true, "city": "Dallas" },
-    { "name": "Charlie Smith", "email": "charlies@c.com", "password": "123456", "industry": "Space Exploration", "recruiterStatus": true, "city": "San Francisco" },
-    { "name": "Janette Tinoco", "email": "janettet@j.com", "password": "123456", "industry": "Dev", "recruiterStatus": false, "city": "San Francisco" },
-    { "name": "George Clooni", "email": "georgec@g.com", "password": "123456", "industry": "Biotech", "recruiterStatus": false, "city": "San Francisco" },
-    { "name": "Peter Parker", "email": "peterp@p.com", "password": "123456", "industry": "Charcoal Mining", "recruiterStatus": false, "city": "San Francisco" },
-    { "name": "James Kerry", "email": "jamesk@j.com", "password": "123456", "industry": "Political Science", "recruiterStatus": false, "city": "Dallas" },
-    { "name": "Mark Verizen", "email": "markv@m.com", "password": "123456", "industry": "Software Development", "recruiterStatus": false, "city": "New York" },
-    { "name": "Ashley Smith", "email": "ashleys@a.com", "password": "123456", "industry": "Software Engineering", "recruiterStatus": true, "city": "New York" },
-    { "name": "Alexey Sergeev", "email": "alexeys@a.com", "password": "123456", "industry": "Software Engineering", "recruiterStatus": false, "city": "San Francisco" },
-  ]
-  
 
-  
-  
-  //bcrypt the password & save to DB
-  let newUser;
-  users.forEach(user => {  
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(user.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser = new User(user);
-          newUser.password = hash;
-          newUser.save()
-        })
-      })
-  })
-}
+  let users = [
+   { "name": "Jonathan Diaz", "email": "jonathand@j.com", "password": "123456", "industry": "Ape Industries", "recruiterStatus": false, "city": "New York" },
+   { "name": "Cindy Vega", "email": "cindyv@c.com", "password": "123456", "industry": "Software Engineering", "recruiterStatus": false, "city": "New York" },
+   { "name": "George Washington", "email": "georgew@g.com", "password": "123456", "industry": "Wood Chopping", "recruiterStatus": true, "city": "San Francisco" },
+   { "name": "Michelle Obama", "email": "michelleo@m.com", "password": "123456","industry": "Political Science", "recruiterStatus": false, "city": "New York" },
+   { "name": "Thomas Jefferson", "email": "thomasj@t.com", "password": "123456","industry": "Iron", "recruiterStatus": false, "city": "Dallas" },
+   { "name": "Martha Stewart", "email": "marthas@m.com", "password": "123456", "industry": "Food Industry", "recruiterStatus": false, "city": "Dallas" },
+   { "name": "Erick Guzi", "email": "erickg@e.com", "password": "123456", "industry": "Pharma", "recruiterStatus": true, "city": "Dallas" },
+   { "name": "Charlie Smith", "email": "charlies@c.com", "password": "123456", "industry": "Space Exploration", "recruiterStatus": true, "city": "San Francisco" },
+   { "name": "Janette Tinoco", "email": "janettet@j.com", "password": "123456", "industry": "Dev", "recruiterStatus": false, "city": "San Francisco" },
+   { "name": "George Clooni", "email": "georgec@g.com", "password": "123456", "industry": "Biotech", "recruiterStatus": false, "city": "San Francisco" },
+   { "name": "Peter Parker", "email": "peterp@p.com", "password": "123456", "industry": "Charcoal Mining", "recruiterStatus": false, "city": "San Francisco" },
+   { "name": "James Kerry", "email": "jamesk@j.com", "password": "123456", "industry": "Political Science", "recruiterStatus": false, "city": "Dallas" },
+   { "name": "Mark Verizen", "email": "markv@m.com", "password": "123456", "industry": "Software Development", "recruiterStatus": false, "city": "New York" },
+   { "name": "Ashley Smith", "email": "ashleys@a.com", "password": "123456", "industry": "Software Engineering", "recruiterStatus": true, "city": "New York" },
+   { "name": "Alexey Sergeev", "email": "alexeys@a.com", "password": "123456", "industry": "Software Engineering", "recruiterStatus": false, "city": "San Francisco" },
+ ]
+
+// for ()
+//   if (i < users.length) {
+//     console.log(i)
+//     console.log(users[i].password)
 
 
-module.exports = doSeeds;
+
+//     users.forEach(user => {  
+//         bcrypt.genSalt(10, (err, salt) => {
+//           bcrypt.hash(user.password, salt, (err, hash) => {
+//             if (err) throw err;
+//             // newUser = new User(user);
+//             // newUser.password = hash;
+//             user.password = hash
+//             console.log(user.password)
+//             // newUser.save()
+//           })
+//         })
+//         asyncLoop( i + 1, callback)
+//     })
+//   } else {
+//     callback()
+//   }
+// }
+  
+// asyncLoop( 0, function() {
+//   // console.log(users)
+// })
+  
+  
+// // }
+
+console.log(users)
+
+
+// module.exports = doSeeds;
