00d59aa73faa0f6f88c4e7507c8d48e77c4ee3ee
AlexeyS <xengawe@gmail.com>
2021-02-26 23:22:25 -0800

    work on seeds

--- seed/faker_seeds.js
+++ seed/faker_seeds.js
@@ -1,5 +1,5 @@
 const faker = require('faker')
-
+const detect = require('detect-gender');
 
 let arr = []
 
@@ -31,8 +31,10 @@ for (i = 0; i < 20; i++){
   user.industry = industry[Math.floor(Math.random() * industry.length)]
 
   //create occupation
+  // user.jobtitle = faker.name.jobTitle()
   let occupation = ["CTO", "CEO", "HR", "Accountant", "Engineer", "SWE", "QA",
-    "Assistant", "Agent", "Coordinator", "Developer"]
+    "Assistant", "Agent", "Coordinator", "Developer","Backend Developer", "Full Stack",
+    "Frontend Developer"]
   user.occupation = occupation[Math.floor(Math.random() * occupation.length)]
 
   //create city
@@ -48,9 +50,55 @@ for (i = 0; i < 20; i++){
   user.linkedIn = 'http://linkedIn.com/some_random_id';
 
   //aboutMe
-  let aboutMe = ["Hello, I am looking for new friends.", "Hello, how are you?"]
+  let aboutMe = ["Hello, I am looking for new friends.", "Hello, how are you?", 
+    "Hello, how are you?", "Hello, tell me a joke.", "Hello, I love running.", 
+    "Hello, I am cat person.", "Hello, I am dog person",  "Hello, I am foody.", 
+    "Hello, I am musician", "Hello, I am optimist.", "Hello, lets go for a walk.", 
+    "Hello, I love water.", "Hello, I here. Add me.",  "Hello, I am new to this.",
+    "Hello, I am excited.", "Hello, I am not alone :D", "I am feeling good today"]
+  user.aboutMe = aboutMe[Math.floor(Math.random() * aboutMe.length)]
+
+  //recruiterStatus
+  let recruiterStatus = [true, false]
+  user.recruiterStatus = recruiterStatus[Math.floor(Math.random() * recruiterStatus.length)]
+
+  //imageUrl
+  let femaleArr = ["https://linkr-dev.s3-us-west-1.amazonaws.com/michelle-obama.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-10.gif",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-11.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-12.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-13.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-14.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-15.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-4.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-5.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-6.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-7.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-8.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/woman-9.jpg"
+  ]
+
+  let maleArr = ["https://linkr-dev.s3-us-west-1.amazonaws.com/batrrack%3Dobama.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-10.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-11.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-12.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-13.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-14.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-4.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-5.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-6.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/man-7.jpg",
+    "https://linkr-dev.s3-us-west-1.amazonaws.com/man8.jpg]",
+  ]
+
+  detect(user.name[0]).then(function (gender) {
+    console.log(gender)
+  });
+
 
   arr.push(user)
 }
 
 console.log(arr)
+
+
