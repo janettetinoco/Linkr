e69367e023a829b2736f851d1a00295cb28dadd4
AlexeyS <xengawe@gmail.com>
2021-02-28 11:22:31 -0800

    add flipping anime

--- frontend/src/components/my_profile/my_profile.jsx
+++ frontend/src/components/my_profile/my_profile.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 class MyProfile extends React.Component{
+  constructor(props){
+    super(props)
+    this.handleEditButton =this.handleEditButton.bind(this)
+  }
 
   handleEditButton(){
+    let h = document.getElementsByClassName('profile-container')
+    if (!h[0].classList.contains('is-flipped')){
+      h[0].classList.add('is-flipped')
+    } else {
+      h[0].classList.remove('is-flipped')
+    }
   }
 
   componentDidMount(){
@@ -26,46 +36,66 @@ class MyProfile extends React.Component{
       <div className='profile-page'>
         <div className='profile-container'>
           <div className='card'>
-            <div className='image-container'>
-              <img id='img-main' src={this.props.self.imageUrl} />
-              <div className='edit-b'>
-                <button onClick={this.props.handleEditButton}><img id='edit' className='user-icon' src="https://img.icons8.com/ultraviolet/40/000000/edit.png" /></button>
-                <h1>Edit Profile</h1>
+            <div className="flip-card-front">
+              <div className='image-container'>
+                <img id='img-main' src={this.props.self.imageUrl} />
+                <div className='edit-b'>
+                  <button onClick={this.handleEditButton}><img id='edit' className='user-icon' src="https://img.icons8.com/ultraviolet/40/000000/edit.png" /></button>
+                  <h1>Edit Profile</h1>
+                </div>
+              </div>
+              <div className='user-details-name'>
+                <h1>{this.props.self.name}</h1>
+              </div>
+              <div className='user-details'>
+                <p className='user-info loc' ><img className='user-icon' src="https://img.icons8.com/cotton/64/000000/worldwide-location--v2.png" />{this.props.self.city}</p>
+              </div>
+              <div className='user-details'>
+                <p className='user-info' >Education<img className='user-icon' src="https://img.icons8.com/cotton/64/000000/graduation-cap--v2.png" /></p>
+                <h2>{this.props.self.education}</h2>
+              </div>
+              <div className='user-details'>
+                <p className='user-info' >Occupation<img className='user-icon' src="https://img.icons8.com/cotton/64/000000/briefcase--v1.png" /></p>
+                <h2>{this.props.self.occupation}</h2>
+              </div>
+              <div className='user-details'>
+                <p className='user-info'  >Industry<img className='user-icon' src="https://img.icons8.com/cotton/64/000000/company.png" /></p>
+                <h2>{this.props.self.industry}</h2>
+              </div>
+              <div className='user-details'>
+                <p className='user-info' >About Me<img  className='user-icon' src="https://img.icons8.com/cotton/64/000000/name--v2.png" /></p>
+                <h2>{this.props.self.aboutMe}</h2>
+              </div>
+              <div className='user-details'>
+                <p className='user-info' >Links<img  className='user-icon' src="https://img.icons8.com/cotton/64/000000/external-link.png" /></p>
+                <div className='links'> 
+                  <img className='links img' src="https://img.icons8.com/color/48/000000/linkedin.png" />
+                  <h2><a href='http://linkedin.com'>{this.props.self.linkedIn}</a></h2> 
+                </div>
               </div>
             </div>
-            <div className='user-details-name'>
-              <h1>{this.props.self.name}</h1>
-            </div>
-            <div className='user-details'>
-              <p className='user-info loc' ><img className='user-icon' src="https://img.icons8.com/cotton/64/000000/worldwide-location--v2.png" />{this.props.self.city}</p>
-            </div>
-            <div className='user-details'>
-              <p className='user-info' >Education<img className='user-icon' src="https://img.icons8.com/cotton/64/000000/graduation-cap--v2.png" /></p>
-              <h2>{this.props.self.education}</h2>
-            </div>
-            <div className='user-details'>
-              <p className='user-info' >Occupation<img className='user-icon' src="https://img.icons8.com/cotton/64/000000/briefcase--v1.png" /></p>
-              <h2>{this.props.self.occupation}</h2>
-            </div>
-            <div className='user-details'>
-              <p className='user-info'  >Industry<img className='user-icon' src="https://img.icons8.com/cotton/64/000000/company.png" /></p>
-              <h2>{this.props.self.industry}</h2>
-            </div>
-            <div className='user-details'>
-              <p className='user-info' >About Me<img  className='user-icon' src="https://img.icons8.com/cotton/64/000000/name--v2.png" /></p>
-              <h2>{this.props.self.aboutMe}</h2>
-            </div>
-            <div className='user-details'>
-              <p className='user-info' >Links<img  className='user-icon' src="https://img.icons8.com/cotton/64/000000/external-link.png" /></p>
-              <div className='links'> 
-                <img className='links img' src="https://img.icons8.com/color/48/000000/linkedin.png" />
-                <h2><a href='http://linkedin.com'>{this.props.self.linkedIn}</a></h2> 
+            <div className='flip-card-back'>
+              <h1>Update Profile</h1>
+              <div className='image-container'>
+                <img id='img-main' src={this.props.self.imageUrl} />
+                <div className='edit-b'>
+                  <button onClick={this.handleEditButton}><img id='edit' className='user-icon' src="https://img.icons8.com/wired/64/000000/left.png" /></button>
+                  <h1>Go back</h1>
+                </div>
               </div>
+              <input type="text" value={this.props.self.name} />
+              <input type="text" value={this.props.self.city} />
+              <input type="text" value={this.props.self.education} />
+              <input type="text" value={this.props.self.occupation} />
+              <input type="text" value={this.props.self.industry} />
+              <input type="text" value={this.props.self.aboutMe} />
+              <input type="text" value={this.props.self.linkedIn}/>
             </div>
           </div>
         </div>
       </div>
     )}
+
 }
 
 export default MyProfile;
