5420f8355137aa42e148d199050f3c9db8880480
janettetinoco <52670122+janettetinoco@users.noreply.github.com>
2021-03-16 20:22:43 -0700

    fix modal

--- frontend/src/components/connections/welcome_tutorial.js
+++ frontend/src/components/connections/welcome_tutorial.js
@@ -6,13 +6,17 @@ class WelcomeTutorial extends React.Component {
     render() {
         
         return (
-            <div className="container">
+            <div className="welcome-container">
                 <div className="content">
                     <h1 className="frame-1"> Welcome to Linkr!<br/><span>Ready to connect?</span></h1>
-                    <h1 className="frame-2">Ready to connect?</h1>
-                    <h1 className="frame-3">Simply browse through the main page</h1>
-                    <h1 className="frame-4">Swipe RIGHT if you would like to connect</h1>
-                    <h1 className="frame-5">Swipe LEFT if you want to skip to the next</h1>
+                    <h1 className="frame-2">Simply browse through the main page</h1>
+                    <h1 className="frame-3">
+                        Swipe <span>right</span> if you would like to connect
+                        <br /><br/>and<br/><br/>
+                        Swipe <span>left</span> if you want to skip to the next
+                    </h1>
+                    <h1 className="frame-4"></h1>
+                    <h1 className="frame-5"></h1>
                     <h1 className="frame-6">Start Connecting!</h1>
                 </div>
             </div>
